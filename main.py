1bcb1e78021fecb0c9cbe0f67937cf6b1cbf75e6
EthanPassinoFWCS <69813689+EthanPassinoFWCS@users.noreply.github.com>
2020-11-02 15:11:39 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+import importlib
+
+def helloWorld():
+  importlib.import_module("hello_world")
+
+def numbers():
+  importlib.import_module("numbers")
+
+def strings():
+  importlib.import_module("strings")
+
+def assignment():
+  importlib.import_module("assignment")
+
+importlib.import_module("TIY2_9")
+
